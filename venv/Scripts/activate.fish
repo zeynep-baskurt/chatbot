8e85d3110c6fe5fa957894318ecd0fc72acc5da5
ulkugedik <ulkugedik3579@gmail.com>
2026-08-13 13:57:38 +0300

    update

--- venv/Scripts/activate.fish
+++ venv/Scripts/activate.fish
@@ -33,7 +33,7 @@ end
 # Unset irrelevant variables.
 deactivate nondestructive
 
-set -gx VIRTUAL_ENV 'C:\Users\zeyne\Desktop\chatbot\venv'
+set -gx VIRTUAL_ENV 'C:\Users\msi\Desktop\chatbot\venv'
 
 set -gx _OLD_VIRTUAL_PATH $PATH
 set -gx PATH "$VIRTUAL_ENV/"Scripts $PATH
