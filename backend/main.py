d20a13045b5008d0305c50b195770c417912b7bc
ulkugedik <ulkugedik3579@gmail.com>
2026-08-17 22:59:16 +0300

    update

--- backend/main.py
+++ backend/main.py
@@ -20,7 +20,7 @@ app = FastAPI(
 
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=["*"],
+    allow_origins=["http://localhost:8080"],
     allow_credentials=True,
     allow_methods=["*"],
     allow_headers=["*"],
